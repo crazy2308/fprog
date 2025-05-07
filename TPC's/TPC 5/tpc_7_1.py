d3acb1efd913f075590244ab58e017e84c71e33c
Mexaformio <jpllima2306@gmail.com>
2025-05-07 23:41:57 +0100

    Tpc

---
+++ TPC's/TPC 5/tpc_7_1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  7 22:11:41 2025
+
+@author: Utilizador
+"""
+
+class Esfera:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return self.radius**2 * 3,1416 * 4
+
+    def volume(self):
+        return (self.radius**3) * (4/3) * 3,1416
